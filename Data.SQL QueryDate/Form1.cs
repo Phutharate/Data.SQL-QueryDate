fbb80cc2e65a471c5414f4b965e809dbaca79835
Phutharate <phutharateheart@gmail.com>
2024-11-18 15:58:32 +0700

    l

--- Data.SQL QueryDate/Form1.cs
+++ Data.SQL QueryDate/Form1.cs
@@ -24,7 +24,7 @@ namespace Data.SQL_QueryDate
             conn = new SqlConnection(strCon);
             conn.Open();
         }
-        //
+        
 
         private void disconnect()
         {
